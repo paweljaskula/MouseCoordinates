e611112da4cf7b4bc14d6857c900464b437ebf79
Pawel Jaskula <pj.jaskula@gmail.com>
2017-05-13 15:00:19 +0200

    Program
    Add Program class with main

---
+++ Program.java
@@ -0,0 +1,13 @@
+package MouseMotion;
+
+
+public class Program {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		Frame frame1 = new Frame();
+		frame1.setDefaultCloseOperation(3);
+		frame1.setVisible(true);
+	}
+
+}
